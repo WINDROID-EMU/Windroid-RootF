37d5acc3738f23c6b399e38b4dfd9460d664eb36
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-05-19 04:36:59 -0300

    build.sh
    Wine HQ teste

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,74 +1,46 @@
 #!/bin/bash
-set -e
 
-package="wine"
-INSTALL_DIR="${INSTALL_DIR:-$PWD/output/pkg/$package}"
-INIT_DIR="${INIT_DIR:-$PWD}"
-TOOLCHAIN_TRIPLE="${TOOLCHAIN_TRIPLE:-x86_64-linux-gnu}" # alvo x86_64 Linux
-PREFIX="$INSTALL_DIR/files/wine"
-JOBS=$(nproc)
+# Wine Proton compilável e empacotável para Windroid RootFS como .rat
 
-GIT_URL="https://gitlab.winehq.org/wine/wine.git"
-GIT_BRANCH="staging"
-GIT_COMMIT=""
+PKG_VER="proton-9.0"
+PKG_CATEGORY="Wine"
+PKG_PRETTY_NAME="Wine Proton (Valve)"
+BLACKLIST_ARCH=aarch64
+
+GIT_URL="https://github.com/ValveSoftware/wine.git"
+GIT_COMMIT="proton-9.0"
+
+OVERRIDE_PREFIX="$INSTALL_DIR/files/wine"
+
+HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
+HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
+HOST_BUILD_MAKE="make -j$(nproc) __tooldeps__ nls/all"
+
+CONFIGURE_ARGS="--enable-archs=i386,x86_64 \
+  --host=$TOOLCHAIN_TRIPLE \
+  --with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
+  --prefix=$OVERRIDE_PREFIX \
+  --disable-tests \
+  --disable-winemenubuilder \
+  --disable-win16 \
+  --without-oss \
+  --with-x \
+  --x-libraries=$PREFIX/lib \
+  --x-includes=$PREFIX/include \
+  --with-pulse \
+  --with-opengl \
+  --with-gstreamer \
+  --with-gnutls \
+  --enable-esync \
+  --enable-fsync \
+  --with-xinput \
+  --with-xinput2 \
+  --enable-nls"
 
 pre_build() {
-    echo "[wine] Clonando WineHQ..."
-    if [ ! -d wine_src ]; then
-        git clone --depth 1 --branch $GIT_BRANCH $GIT_URL wine_src
-    else
-        cd wine_src && git fetch && git checkout $GIT_BRANCH && git pull && cd ..
-    fi
-}
-
-build_wine() {
-    mkdir -p wine_build
-    cd wine_build
-
-    echo "[wine] Configurando WineHQ para Windroid x86_64..."
-    ../wine_src/configure \
-        --host=$TOOLCHAIN_TRIPLE \
-        --build=x86_64-linux-gnu \
-        --prefix=$PREFIX \
-        --enable-win64 \
-        --disable-tests \
-        --disable-win16 \
-        --enable-esync \
-        --enable-fsync \
-        --with-pulse \
-        --with-x \
-        --with-opengl \
-        --with-gstreamer \
-        --with-gnutls \
-        --with-xinput \
-        --with-xinput2 \
-        CFLAGS="-O2" \
-        CXXFLAGS="-O2"
-
-    echo "[wine] Compilando WineHQ x86_64..."
-    make -j$JOBS
-    make install
-
-    cd ..
+  echo "Iniciando build do Wine Proton para Windroid RootFS (.rat)..."
 }
 
 post_build() {
-    echo "[wine] Corrigindo permissões para usuário u0_a273..."
-    chown -R u0_a273:u0_a273 $PREFIX
-
-    echo "[wine] Empacotando Wine em .rat..."
-
-    cd $INSTALL_DIR/files
-
-    zip -r ../winehq-x86_64-latest.rat wine
-
-    echo "[wine] Build e empacotamento finalizados com sucesso."
+  echo "Wine Proton instalado corretamente em: $OVERRIDE_PREFIX"
 }
-
-main() {
-    pre_build
-    build_wine
-    post_build
-}
-
-main "$@"
