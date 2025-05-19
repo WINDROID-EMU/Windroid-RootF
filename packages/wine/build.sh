8962871ef2739ec59dc7fe2fa77d1e061664203e
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-05-19 03:42:22 -0300

    build.sh
    wine original

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,45 +1,48 @@
-PKG_VER="proton-experimental"
+PKG_VER="10.1-9-esync-xinput"
 PKG_CATEGORY="Wine"
-PKG_PRETTY_NAME="Wine (Proton)"
+PKG_PRETTY_NAME="Wine ($PKG_VER)"
+
 BLACKLIST_ARCH=aarch64
 
-GIT_URL="https://github.com/ValveSoftware/wine.git"
-GIT_COMMIT="proton-9.0"  # ou use uma tag exata como wine-valve-8.0
-
-OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
-
+GIT_URL=https://github.com/KreitinnSoftware/wine
+GIT_COMMIT=36b176851ffce636fc052fab773fb2be8990fe5c
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
-HOST_BUILD_MAKE="make -j$(nproc) __tooldeps__ nls/all"
-
+HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
+OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
 CONFIGURE_ARGS="--enable-archs=i386,x86_64 \
-  --host=$TOOLCHAIN_TRIPLE \
-  --with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
-  --prefix=$OVERRIDE_PREFIX \
-  --without-oss \
-  --disable-tests \
-  --disable-winemenubuilder \
-  --disable-win16 \
-  --with-x \
-  --x-libraries=$PREFIX/lib \
-  --x-includes=$PREFIX/include \
-  --with-pulse \
-  --with-opengl \
-  --with-gstreamer \
-  --with-gnutls \
-  --with-xinput \
-  --with-xinput2 \
-  --enable-nls \
-  --enable-esync \
-  --enable-fsync"
-
-pre_build() {
-  echo "Preparando build do Wine Proton (Valve)..."
-
-  # Opcional: aplicar patches Proton aqui se necessário
-  # patch -p1 < "$INIT_DIR/patches/proton/wine-proton-fixes.patch"
-}
-
-post_build() {
-  echo "Wine Proton compilado com sucesso."
-}
+				--host=$TOOLCHAIN_TRIPLE \
+				--with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
+				--prefix=$OVERRIDE_PREFIX \
+				--without-oss \
+				--disable-winemenubuilder \
+				--disable-win16 \
+				--disable-tests \
+				--with-x \
+				--x-libraries=$PREFIX/lib \
+				--x-includes=$PREFIX/include \
+				--with-pulse \
+				--with-gstreamer \
+				--with-opengl \
+				--with-gnutls \
+				--with-mingw=gcc \
+				--with-xinput \
+				--with-xinput2 \
+    				--enable-nls \
+				--without-xshm \
+				--without-xxf86vm \
+				--without-osmesa \
+				--without-usb \
+				--without-sdl \
+				--without-cups \
+				--without-netapi \
+				--without-pcap \
+				--without-gphoto \
+				--without-v4l2 \
+				--without-pcsclite \
+				--without-wayland \
+				--without-opencl \
+				--without-dbus \
+				--without-sane \
+				--without-udev \
+				--without-capi"
