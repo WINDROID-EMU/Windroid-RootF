000be1742f5fea98fd233ce9aa5b27460ba46f48
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-05-19 03:40:45 -0300

    build.sh
    teste wine hq

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,48 +1,40 @@
-PKG_VER="10.1-9-esync-xinput"
+PKG_VER="git-master"
 PKG_CATEGORY="Wine"
 PKG_PRETTY_NAME="Wine ($PKG_VER)"
-
 BLACKLIST_ARCH=aarch64
 
-GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=36b176851ffce636fc052fab773fb2be8990fe5c
+GIT_URL="https://gitlab.winehq.org/wine/wine.git"
+GIT_COMMIT="master"
+
+OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
+
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
-HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
-OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
+HOST_BUILD_MAKE="make -j$(nproc) __tooldeps__ nls/all"
+
 CONFIGURE_ARGS="--enable-archs=i386,x86_64 \
-				--host=$TOOLCHAIN_TRIPLE \
-				--with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
-				--prefix=$OVERRIDE_PREFIX \
-				--without-oss \
-				--disable-winemenubuilder \
-				--disable-win16 \
-				--disable-tests \
-				--with-x \
-				--x-libraries=$PREFIX/lib \
-				--x-includes=$PREFIX/include \
-				--with-pulse \
-				--with-gstreamer \
-				--with-opengl \
-				--with-gnutls \
-				--with-mingw=gcc \
-				--with-xinput \
-				--with-xinput2 \
-    				--enable-nls \
-				--without-xshm \
-				--without-xxf86vm \
-				--without-osmesa \
-				--without-usb \
-				--without-sdl \
-				--without-cups \
-				--without-netapi \
-				--without-pcap \
-				--without-gphoto \
-				--without-v4l2 \
-				--without-pcsclite \
-				--without-wayland \
-				--without-opencl \
-				--without-dbus \
-				--without-sane \
-				--without-udev \
-				--without-capi"
+  --host=$TOOLCHAIN_TRIPLE \
+  --with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
+  --prefix=$OVERRIDE_PREFIX \
+  --without-oss \
+  --disable-winemenubuilder \
+  --disable-tests \
+  --disable-win16 \
+  --with-x \
+  --x-libraries=$PREFIX/lib \
+  --x-includes=$PREFIX/include \
+  --with-pulse \
+  --with-opengl \
+  --with-gstreamer \
+  --with-gnutls \
+  --with-xinput \
+  --with-xinput2 \
+  --enable-nls"
+
+pre_build() {
+  echo "Preparando ambiente para Wine oficial (WineHQ)..."
+}
+
+post_build() {
+  echo "Wine oficial compilado e instalado em: $OVERRIDE_PREFIX"
+}
